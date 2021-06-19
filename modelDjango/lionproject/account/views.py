734b58f0b9b61e7fc1fda34a6c75a332042479e6
portfolio_1st <aa991122aa@gmail.com>
2021-06-20 02:28:23 +0900

    로그인 로그아웃기능 추가

---
+++ modelDjango/lionproject/account/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST' :
+        form=AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username=form.cleaned_data.get("username")
+            password=form.cleaned_data.get("password")
+            user = authenticate(request=request,username=username, password=password)
+            if user is not None : #user가 존재할떄
+                login(request,user)
+            return redirect("home")
+    else :
+        form=AuthenticationForm()
+        return render(request, 'login.html',{'form':form})
+        
+def logout_view(request):
+    logout(request)
+    return redirect("home")
